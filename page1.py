7c0e32eced9ebcb825b2dff58851bc8413e3b290
Ahtsham0715 <ahtsham50743@gmail.com>
2022-05-13 19:57:38 +0500

    page1 completely redesigned.

--- page1.py
+++ page1.py
@@ -1,54 +1,43 @@
 
-import tkinter as tk
-import tkinter.font as tkFont
+from tkinter import *
+
+
 
 from page2 import my_page2
 def my_page1():
     #setting title
-    root = tk.Tk()
+    root = Tk()
     root.title("Microsoft Essential Document Reader")
-    #setting window size
-    width=600
-    height=500
-    screenwidth = root.winfo_screenwidth()
-    screenheight = root.winfo_screenheight()
-    alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
-    root.geometry(alignstr)
+    root.geometry('850x550')
+    root.config(bg = 'white')
     root.resizable(width=False, height=False)
     root.overrideredirect(True)
     
-    GLabel_873=tk.Label(root)
-    ft = tkFont.Font(family='Times',size=18)
-    GLabel_873["font"] = ft
-    GLabel_873["fg"] = "#333333"
-    GLabel_873["justify"] = "center"
-    GLabel_873["text"] = "Department of consumer affairs and national consumer helpline vital\nclarification documents. "
-    GLabel_873.place(x=20,y=70,width=550)
+    moca_img = PhotoImage(file='moca1.png')
 
-    def GButton_240_command():
+    Label(root, image=moca_img, width=300, height=200, bg='white').place(x=270, y=5)
+
+    Label(root, text='Department of consumer affairs and national consumer helpline vital\nclarification documents.',
+        font=('arial', 15, 'bold'), bg='white').place(x=90, y=210)
+
+    
+    Label(root, text='18 Complaints Registered for Business and Trade malpractice, cheating\nand fraud under section 415 of I PC. Proceed to answer the questions for\nclarification and submit self attested digital copy of the same to concerned\nDepartment',
+        font=('arial', 15, 'normal'), bg='white').place(x=80, y=290)
+
+    
+    Label(root, text='Disclaimer. The content of this document is confidential and intended for the recipient specified in the document only. It is strictly forbidden to share any\npart of this document with any third party without a written consent of the victims. Unauthorized use disclosure or copying of this document or any part\nthereof is strictly prohibited and is unlawful',
+        font=('arial', 9, 'normal'), justify=LEFT, bg='white').place(x=5, y=430)
+
+    
+    def btnFunction():
         root.destroy()
         my_page2()
 
-    GButton_240=tk.Button(root)
-    GButton_240["bg"] = "#000000"
-    ft = tkFont.Font(family='Times',size=18)
-    GButton_240["font"] = ft
-    GButton_240["fg"] = "#ffffff"
-    GButton_240["justify"] = "center"
-    GButton_240["text"] = "Proceed"
-    GButton_240.place(x=490,y=450,width=100)
-    GButton_240["command"] = GButton_240_command
-
-    GLabel_231=tk.Label(root)
-    ft = tkFont.Font(family='Times',size=14)
-    GLabel_231["font"] = ft
-    GLabel_231["fg"] = "#333333"
-    GLabel_231["justify"] = "center"
-    GLabel_231["text"] = "18 Complaints Registered for Business and Trade malpractice, cheating\nand fraud under section 415 of I PC. Proceed to answer the questions for\nclarification and submit self attested digital copy of the same to concerned\nDepartment "
-    GLabel_231.place(x=20,y=150,width=570)
+    Button(root, text=' Proceed ', bg='black',fg = 'white', font=(
+        'arial', 12, 'normal'), command=btnFunction).place(x=760, y=510)
+    
     root.mainloop()
     
     
-
-
+# my_page1()
     
